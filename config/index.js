79d87b9f425927587713825e55aa1320392c9e36
12cgh <114244834+12cgh@users.noreply.github.com>
2022-09-23 23:52:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oT_1X65Zb5khakMeDY--NM9V1oVw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "EMfHivrtj0GEFWBBr-BO3aayPf5-_aHZK-8PMY0pz_s	",
+      useTemplateId: "EMfHivrtj0GEFWBBr-BO3aayPf5-_aHZK-8PMY0pz_s",
       // 所在省份
       province: "安徽",
       // 所在城市
@@ -97,7 +97,7 @@ export const USER_CONFIG = {
     {
       name: "叶林华",
       id: "oT_1X65Zb5khakMeDY--NM9V1oVw",
-      useTemplateId: "EMfHivrtj0GEFWBBr-BO3aayPf5-_aHZK-8PMY0pz_s	",
+      useTemplateId: "EMfHivrtj0GEFWBBr-BO3aayPf5-_aHZK-8PMY0pz_s",
       province: "",
       city: "",
       horoscopeDate: '',
